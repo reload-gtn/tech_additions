9909c369124560581a2b30562267d6845e984840
Aleksandr <aleksandr@cultLinuxPC>
2023-10-02 17:10:18 +0000

    add iron ingot block

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
-minetest.register_node("tech_additions:iron_block", {
-    description = "Block TA1 Iron",
-    tiles = { 'tech_additions_iron_block.png' }
+minetest.register_node("tech_additions:iron_ingot_block", {
+    description = "Block TA1 Iron Ingot",
+    tiles = { 'tech_additions_iron_ingot_block.png' }
 })
 
 minetest.register_craft({
-    output = 'tech_additions:iron_block',
+    output = 'tech_additions:iron_ingot_block',
     recipe = {
         { "techage:iron_ingot", "techage:iron_ingot", "techage:iron_ingot", },
         { "techage:iron_ingot", "techage:iron_ingot", "techage:iron_ingot", },
@@ -15,6 +15,6 @@ minetest.register_craft({
 minetest.register_craft({
     output = 'techage:iron_ingot 9',
     recipe = {
-		{'tech_additions:iron_block',},
+		{'tech_additions:iron_ingot_block',},
 	}
 })
